dc6bd30d72cf344078d43b1b7d3bc3bcccccc92f
gomezgonzalo2012 <gomezgonzalo2012@gmail.com>
2024-04-27 01:28:59 -0300

    Adding ConsoleApp2 and class lib. Person.Core.Entities

---
+++ unit1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using Person.Core.Entities;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h1 = new Human();
+            h1.Name = "Jane";
+            h1.LastName = "Doe";
+            h1.Age = 30;
+
+            Console.WriteLine($"Name: {h1.Name} {h1.LastName} Age: {h1.Age}");
+        }
+    }
+}
