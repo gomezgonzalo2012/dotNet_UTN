f94508df7554c9976b089b045e499b61b8692e09
gomezgonzalo2012 <gomezgonzalo2012@gmail.com>
2024-04-27 01:07:49 -0300

    Creating the project

---
+++ unit1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.AccessControl;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            System.Console.WriteLine("Enter any key and see magic");
+            string? input = Console.ReadLine();
+           for (int i = 0;i<5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(input);
+                    
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
